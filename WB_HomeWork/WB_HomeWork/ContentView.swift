8f780a2e151c991bb5d411354680963dbceaf3bf
Sergey <maybah__94@mail.ru>
2024-07-22 03:43:18 +0600

    initial project

---
+++ WB_HomeWork/WB_HomeWork/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WB_HomeWork
+//
+//  Created by Мария Изюменко on 22.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WB_HomeWork/WB_HomeWork/WB_HomeWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WB_HomeWorkApp.swift
+//  WB_HomeWork
+//
+//  Created by Мария Изюменко on 22.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WB_HomeWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
